9c1231ed6d881007e6be3fd1cdf25c388025823b
jorgeguedess <jorge_guedes00@hotmail.com>
2024-05-17 20:43:08 -0300

    feat: add initial base Login component

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,9 @@
+export default function Login() {
+  return (
+    <div className="flex w-full flex-1 flex-col items-center justify-center bg-white">
+      <div className="container mx-auto text-center">
+        <h1 className="text-2xl">Página de Login</h1>
+      </div>
+    </div>
+  );
+}
