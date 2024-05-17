2c335fff9942664d495cc118c16b0f317c3f6235
jorgeguedess <jorge_guedes00@hotmail.com>
2024-05-17 20:43:56 -0300

    feat: implemented react-router-dom in the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import Home from "./pages/home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { Footer } from "./components/footer/Footer";
 import { Header } from "./components/header/Header";
 
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import { NotFound } from "./components/not-found/NotFound";
+
 function App() {
   return (
-    <div>
-      <Header />
-      <Home />
-      <Footer />
-    </div>
+    <>
+      <BrowserRouter>
+        <div className="flex min-h-screen flex-col bg-indigo-900">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
