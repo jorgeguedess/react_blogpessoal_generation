19e64e7deb0a2bdd987727145362dc6db10f43e3
jorgeguedess <jorge_guedes00@hotmail.com>
2024-05-20 19:10:08 -0300

    feat: added Header mobile and route validation

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,43 +1,85 @@
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { List, X } from "@phosphor-icons/react";
+import { links } from "../../data/linksMenu";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export const Header = () => {
+  const navigate = useNavigate();
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { handleLogout, usuario } = useContext(AuthContext);
+
+  const handleToggleMenu = () => {
+    setIsMenuOpen((prevMenu) => !prevMenu);
+  };
+
+  const handleCloseMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  function logout() {
+    handleCloseMenu();
+    handleLogout();
+    alert("O Usuário foi desconectado com sucesso!");
+    navigate("/login");
+  }
+
+  if (!usuario.token) return null;
+
   return (
     <header
       className="flex w-full justify-center
   bg-indigo-900 py-4 text-white"
     >
-      <div className="container flex justify-between text-lg">
-        <Link to="/" className="text-2xl font-bold">
+      <div className="justify-centerx-auto container flex flex-wrap items-center justify-between gap-2 px-4 text-lg">
+        <Link to="/" className="text-2xl font-bold" onClick={handleCloseMenu}>
           Blog Pessoal
         </Link>
 
-        <nav>
-          <ul className="flex gap-4">
-            <li>
-              <Link to="/postagens" className="hover:underline">
-                Postagens
-              </Link>
-            </li>
-            <li>
-              <Link to="/temas" className="hover:underline">
-                Temas
-              </Link>
-            </li>
-            <li>
-              <Link to="/cadastrartema" className="hover:underline">
-                Cadastrar tema
-              </Link>
-            </li>
-            <li>
-              <Link to="/perfil" className="hover:underline">
-                Perfil
-              </Link>
-            </li>
-            <li>
-              <Link to="/login" className="hover:underline">
+        <div className="flex items-center md:hidden">
+          <button
+            type="button"
+            className="ml-1 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 lg:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            onClick={handleToggleMenu}
+            aria-label={isMenuOpen ? "Fechar menu" : "Abrir menu"}
+          >
+            {isMenuOpen ? (
+              <X className="h-6 w-6 text-white" />
+            ) : (
+              <List className="h-6 w-6 text-white" />
+            )}
+          </button>
+        </div>
+
+        <nav
+          className={`absolute left-0 top-16 h-fit w-full transform bg-indigo-800 transition-transform duration-300 ease-in-out ${
+            isMenuOpen ? "translate-x-0" : "-translate-x-full"
+          } md:static md:flex md:w-auto md:translate-x-0 md:bg-transparent`}
+        >
+          <ul className="flex flex-col items-center justify-center md:flex-row md:gap-4 md:p-0">
+            {links.map((link) => (
+              <li key={link.id} className="flex w-full text-center md:w-fit">
+                <Link
+                  to={link.path}
+                  className="block w-full rounded px-1 py-4 transition-colors hover:bg-indigo-700 focus-visible:bg-indigo-700 md:py-2 md:hover:bg-indigo-900 md:hover:underline md:focus-visible:bg-indigo-900"
+                  onClick={handleCloseMenu}
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+            <li className="flex w-full text-center md:w-fit">
+              <button
+                onClick={logout}
+                className="block w-full rounded px-1 py-4 transition-colors hover:bg-indigo-700 focus-visible:bg-indigo-700 md:py-2 md:hover:bg-indigo-900 md:hover:underline md:focus-visible:bg-indigo-900"
+              >
                 Sair
-              </Link>
+              </button>
             </li>
+            {usuario.token && (
+              <p className="hidden md:block">Olá {usuario.nome}</p>
+            )}
           </ul>
         </nav>
       </div>
---
+++ src/data/linksMenu.ts
@@ -0,0 +1,22 @@
+export const links = [
+  {
+    id: "posts",
+    name: "Postagens",
+    path: "/postagens",
+  },
+  {
+    id: "themes",
+    name: "Temas",
+    path: "/temas",
+  },
+  {
+    id: "registerTheme",
+    name: "Cadastrar Tema",
+    path: "/cadastrartema",
+  },
+  {
+    id: "profile",
+    name: "Perfil",
+    path: "/perfil",
+  },
+];
